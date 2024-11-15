e2f98649e86d453268e68db5363ff22d40fc6547
Shohag <sjoarder33@gmail.com>
2024-11-15 23:19:35 +0600

    Exam 2

--- lib/main.dart
+++ lib/main.dart
@@ -19,7 +19,7 @@ class MyProfilePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Colors.yellow,
+        backgroundColor: Colors.yellow[700],
         title: Text('My Profile'),
         actions: [
           IconButton(
@@ -40,7 +40,7 @@ class MyProfilePage extends StatelessWidget {
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
           ProfileSection(
-            icon: Icons.icecream,
+            icon: Icons.icecream_outlined,
             text: 'Ice cream is very delicious right?',
           ),
           ProfileSection(
@@ -48,8 +48,8 @@ class MyProfilePage extends StatelessWidget {
             text: 'Programming is not boring if you love it.',
           ),
           ProfileSection(
-            icon: Icons.egg,
-            text: 'If you submit code directly copy from chatgpt, than mark will be 0.',
+            icon: Icons.egg_outlined,
+            text: 'If you submit code directly copy from chatgpt than mark will be 0.',
           ),
         ],
       ),
@@ -66,22 +66,22 @@ class ProfileSection extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Padding(
-      padding: const EdgeInsets.all(16.0),
+      padding: const EdgeInsets.all(12.0),
       child: Column(
         children: [
           CircleAvatar(
-            radius: 40,
-            backgroundColor: Colors.purple[50],
+            radius: 50,
+            backgroundColor: Colors.deepPurple[100],
             child: Icon(
               icon,
-              size: 40,
-              color: Colors.purple,
+              size: 60,
+              color: Colors.purple[900],
             ),
           ),
           SizedBox(height: 10),
           Text(
             text,
-            style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+            style: TextStyle(fontSize: 14, fontWeight: FontWeight.bold),
             textAlign: TextAlign.center,
           ),
         ],
